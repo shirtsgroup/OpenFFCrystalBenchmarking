c88b414f5a74c4bf6f838025ac44ad0b966715ea
qualenal <sam.kennedy5206@gmail.com>
2021-09-23 23:07:35 -0600

    Ran the 2x2x2 supercell generated by PyMol successfully. Need to figure out how to better report state data before and after energyminimization

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from rdkit import Chem
 from openbabel import pybel
 from openff.toolkit.topology import Molecule,Topology
 from openff.toolkit.utils import RDKitToolkitWrapper
-from simtk import unit
 import openmm
+from simtk.openmm import app
+from openmm import unit
 from openmm.app import PDBFile
 from openff.toolkit.typing.engines.smirnoff import ForceField
 
@@ -12,20 +13,39 @@ rdktkw = RDKitToolkitWrapper()
 
 # Loading setup parameters
 forcefield = ForceField('openff_unconstrained-1.1.0.offxml')
-off_mol = Molecule.from_pdb_and_smiles("notebooks/2107460.pdb", "O=C(N)N1c2ccccc2C=Cc2ccccc12")
-pdb_file = PDBFile('notebooks/2107460.pdb')
+# load pdb with one copy of pdb file
+off_mol = Molecule.from_pdb_and_smiles("data/7101899.pdb", "CC1=CN=C(C(=C1OC)C)C[S@@](=O)C2=NC3=C(N2)C=C(C=C3)OC")
+pdb_file = PDBFile('data/7101899_supercell.pdb')
 off_top = Topology.from_openmm(pdb_file.topology, [off_mol])
 system = forcefield.create_openmm_system(off_top)
 integrator = openmm.VerletIntegrator(1*unit.femtoseconds)
 platform = openmm.Platform.getPlatformByName('Reference')
 
 # create simulation and minimize energy
-#simulation = openmm.app.Simulation(pdb_file.topology, system, integrator, platform)
+simulation = openmm.app.Simulation(pdb_file.topology, system, integrator, platform)
 # set initial positions from pdbfile
 positions = pdb_file.getPositions()
 
-#simulation.context.setPositions(positions)
+simulation.context.setPositions(positions)
+pdb_reporter = openmm.app.PDBReporter('data/trajectory.pdb', 1)
+state_data_reporter = openmm.app.StateDataReporter(
+    "data/data.csv",
+    1,
+    step=True,
+    potentialEnergy=True,
+    temperature=True,
+    density=True,
+)
 
-#simulation.saveState('before')
-#simulation.minimizeEnergy()
-#simulation.saveState('after')
+simulation.saveState('initial')
+
+#state_data_reporter.report(simulation, simulation.context.getState())
+simulation.reporters.append(pdb_reporter)
+simulation.reporters.append(state_data_reporter)
+print('Minimizing Energy!')
+simulation.minimizeEnergy()
+simulation.saveState('after')
+
+
+print("simulating??")
+simulation.step(2)
