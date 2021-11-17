3e8e240e36019be220609bb22c2a64671a49bcc9
qualenal <sam.kennedy5206@gmail.com>
2021-11-17 11:36:14 -0700

    Initial run for minimization RMSD for OpenFF 2.0.0 completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import Block
+import os
 import numpy as np
 from rdkit import Chem
 from openbabel import pybel
@@ -10,9 +11,14 @@ from openmm import unit
 from openmm.app import PDBFile
 from openff.toolkit.typing.engines.smirnoff import ForceField
 from openmm.vec3 import Vec3
+import mdtraj
 import warnings
-warnings.simplefilter("ignore")
+import logging
+import pandas as pd
 
+# Warning and Logger Setup
+warnings.simplefilter("ignore")
+logging.basicConfig(filename='errors.log', filemode='w')
 
 # Wrapper and FF setup
 # Use RDKit wrapper
@@ -20,91 +26,75 @@ rdktkw = RDKitToolkitWrapper()
 
 # Loading setup parameters
 forcefield = ForceField('openff-2.0.0.offxml')
+# Load smiles csv file as pandas
+smiles = pd.read_csv('allcod.smi', names=['SMILES', 'COD ID'], sep='\t')
+with open('data/rmsd_values.txt','w') as f:
+    f.write('COD ID\tRMSD\n')
 
-#%%
+for pdb in os.listdir('data/PDB'):
+    try:
+        # load pdb with one copy of pdb file
+        cod_id = pdb.split('.')[0]
+        print(cod_id)
+        try:
+            # get smiles from all_smilies
+            smiles_string = smiles.loc[smiles['COD ID'] == int(cod_id)]['SMILES'].values[0]
+            off_mol = Molecule.from_pdb_and_smiles('data/PDB/' + pdb, smiles_string)
+        except Exception as e:
+            logging.error('PDB/SMILES error with ID %s' % cod_id)
+            logging.error(e)
+            continue
+        try:
+            # load supercell pdb file (2x2x2) into topology
+            pdb_file = PDBFile('data/PDB_supercell/' + cod_id + '_supercell.pdb')
+            off_top = Topology.from_openmm(pdb_file.topology, [off_mol])
+        except Exception as e:
+            logging.error('Topology error with ID %s' % cod_id)
+            logging.error(e)
+            continue
+        # Create MD simulation inputs
+        system = forcefield.create_openmm_system(off_top)
+        integrator = openmm.VerletIntegrator(1*unit.femtoseconds)
+        platform = openmm.Platform.getPlatformByName('Reference')
 
-# load pdb with one copy of pdb file
-off_mol = Molecule.from_pdb_and_smiles('data/7101899.pdb', "CC1=CN=C(C(=C1OC)C)C[S@@](=O)C2=NC3=C(N2)C=C(C=C3)OC")
-# load supercell pdb file (2x2x2) into topology
-pdb_file = PDBFile('data/7101899_supercell.pdb')
-off_top = Topology.from_openmm(pdb_file.topology, [off_mol])
-# Create MD simulation inputs
-system = forcefield.create_openmm_system(off_top)
-integrator = openmm.VerletIntegrator(1*unit.femtoseconds)
-platform = openmm.Platform.getPlatformByName('Reference')
+        try:
+            # create simulation, catch errors
+            simulation = openmm.app.Simulation(pdb_file.topology, system, integrator, platform)
+        except Exception as e:
+            logging.error('Simulation Build error with ID %s' % cod_id)
+            logging.error(e)
+            continue
+        # set initial positions from pdbfile
+        positions = pdb_file.getPositions()
+        simulation.context.setPositions(positions)
 
-#%%
+        # set reporters
+        pdb_reporter = openmm.app.PDBReporter('data/trajectories/' + cod_id + '.pdb', 1)
+        simulation.reporters.append(pdb_reporter)
+        # set positions
+        simulation.context.setPositions(positions)
+        # save state and print initial PE
+        simulation.saveState('data/initial_states/' + cod_id + '_initial.xml')
+        orig_potential = simulation.context.getState(getEnergy=True).getPotentialEnergy()
+        if orig_potential.value_in_unit(unit.kilojoule_per_mole) > 1e24:
+            continue
+        print('Initial Energy ' + str(orig_potential))
+        # Minimize Energy and save final state
+        print('Minimizing Energy!')
+        simulation.minimizeEnergy(maxIterations=100000)
+        min_state = simulation.context.getState(getEnergy=True, getPositions=True, getForces=True)
+        min_potential = min_state.getPotentialEnergy()
+        simulation.saveState('data/final_states/' + cod_id + '_final.xml')
+        print('Final Energy = ' + str(min_potential))
 
-# create simulation
-simulation = openmm.app.Simulation(pdb_file.topology, system, integrator, platform)
-# set initial positions from pdbfile
-positions = pdb_file.getPositions()
-simulation.context.setPositions(positions)
+        simulation.step(1)
 
-#%%
-
-# set reporters
-pdb_reporter = openmm.app.PDBReporter('trajectory.pdb', 1)
-state_data_reporter = openmm.app.StateDataReporter(
-    "data.csv",
-    1,
-    step=True,
-    potentialEnergy=True,
-    temperature=True,
-    density=True,
-)
-simulation.reporters.append(pdb_reporter)
-simulation.reporters.append(state_data_reporter)
-
-#%%
-
-simulation.context.setPositions(positions)
-simulation.saveState('initial')
-orig_potential = simulation.context.getState(getEnergy=True).getPotentialEnergy()
-#state_data_reporter.report(simulation, simulation.context.getState())
-print('Initial Energy ' + str(orig_potential))
-print('Minimizing Energy!')
-simulation.minimizeEnergy()
-min_state = simulation.context.getState(getEnergy=True, getPositions=True, getForces=True)
-min_potential = min_state.getPotentialEnergy()
-print('Final Energy = ' + str(min_potential))
-
-#%%
-
-# Report if system uses periodic boundary conditions and forces used
-print(simulation.system.usesPeriodicBoundaryConditions())
-print(simulation.system.getForces())
-
-#%%
-
-simulation.step(1)
-
-
-#%%
-
-import pymol
-pymol.cmd.load('7101899_supercell.pdb','initial')
-pymol.cmd.load('trajectory.pdb','final')
-rmsd = pymol.cmd.align('final','initial',cycles=0)
-print(rmsd[0])
-
-#%%
-
-# Need block to feed parameters to minimizer, where forces provide 3*n derivatives of energy
-# wrt position, +3 more derivatives of energy wrt box vectors
-# This block is a work in progress
-forces = simulation.context.getState(getForces=True).getForces()
-p_box_pos = simulation.context.getState(getPositions=True, enforcePeriodicBox=True).getPositions()
-
-x0 = np.array([2,0,2,0,0,2]) # a_x, b_x, b_y, c_x, c_y, c_z
-def box_energy(sim,x,positions):
-    # x will be np array of all inputs
-    sim.system.setDefaultPeriodicBoxVectors(Vec3(x[0],0,0),Vec3(x[1],x[2],0),Vec3(x[3],x[4],x[5]))
-    print(sim.system.getForces())
-    new_integrator = openmm.VerletIntegrator(1*unit.femtoseconds)
-    new_sim = openmm.app.Simulation(pdb_file.topology, system, new_integrator, platform)
-    new_sim.context.setPositions(positions)
-    return new_sim.context.getState(getEnergy=True).getPotentialEnergy()
-energy = box_energy(simulation,x0,positions)
-print(energy)
-#print(forces)
+        initial = mdtraj.load_pdb('data/PDB_supercell/' + cod_id + '_supercell.pdb')
+        final = mdtraj.load_pdb('data/trajectories/' + cod_id + '.pdb')
+        rmsd = mdtraj.rmsd(initial, final)
+        with open('data/rmsd_values.txt','a') as f:
+            f.write('%s\t%s\n' % (cod_id, rmsd[0]))
+    except Exception as e:
+        logging.error('Generic Error')
+        logging.error(e)
+        continue
--- supercell_generation.py
+++ supercell_generation.py
@@ -1,4 +1,5 @@
 # Generate 3x3x3 supercells for all pdb files and apply cryst1 modifications
+import math
 
 import pymol
 import os
@@ -6,13 +7,22 @@ from openbabel import pybel
 
 for f in os.listdir('data/PDB'):
     print(f)
+    # load pdb file to get unit cell dimensions and pick # of cells to ensure each dimension is >18 A
+    try:
+        mol = next(pybel.readfile('pdb','data/PDB/' + f))
+    except Exception as e:
+        print(e)
+        continue
+    a_mult = math.ceil(18.0/mol.unitcell.GetA())
+    b_mult = math.ceil(18.0/mol.unitcell.GetB())
+    c_mult = math.ceil(18.0/mol.unitcell.GetC())
     # reinitialize to clear pymol
     pymol.cmd.reinitialize()
     pymol.cmd.load('data/PDB/' + f)
     id = f.split('.')[0]
     # load supercell and run for molecule
     pymol.cmd.do('run https://raw.githubusercontent.com/speleo3/pymol-psico/master/psico/xtal.py')
-    pymol.cmd.do('supercell 3,3,3,' + id)
+    pymol.cmd.do('supercell %s,%s,%s,%s' % (a_mult, b_mult, c_mult, id))
     pymol.cmd.delete(id)
     # Make the supercell file if needed
     path = '/home/qualenal/Scripts/OpenFFCrystalBenchmarking/data/PDB_supercell/'+id+'_supercell.pdb'
@@ -22,9 +32,9 @@ for f in os.listdir('data/PDB'):
         pass
     pymol.cmd.save(path,format='pdb',quiet='0')
     try:
-        mol = next(pybel.readfile('pdb',path))
-        mol.unitcell.SetData(mol.unitcell.GetA()*3, mol.unitcell.GetB()*3, mol.unitcell.GetC()*3, mol.unitcell.GetAlpha(), mol.unitcell.GetBeta(), mol.unitcell.GetGamma())
-        mol.write('pdb',path,overwrite=True)
+        mol_sc = next(pybel.readfile('pdb', path))
+        mol_sc.unitcell.SetData(mol_sc.unitcell.GetA() * a_mult, mol_sc.unitcell.GetB() * b_mult, mol_sc.unitcell.GetC() * c_mult, mol_sc.unitcell.GetAlpha(), mol_sc.unitcell.GetBeta(), mol_sc.unitcell.GetGamma())
+        mol_sc.write('pdb', path, overwrite=True)
     except AttributeError:
         print(id)
 
